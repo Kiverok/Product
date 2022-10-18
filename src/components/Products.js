48be9efc221a5dea624dc76cc841ef8e9da2515b
Kiverok <kiver566@ukr.net>
2022-10-18 20:05:08 +0300

    error

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,26 +5,33 @@ import axios from "axios";
 
 function Products() {
     const [products, setProducts] = useState([]);
-  const url = 'https://fakestoreapi.com/products?limit=5'
+  // const url = 'https://fakestoreapi.com/products?limit=5'
+  // const query = "5";
+  // const templateUrl = `https://fakestoreapi.com/products?limit=${query}`
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(url);
+  const query = "5";
+  const templateUrl = `https://fakestoreapi.com/products?limit=${query}`
+  const fetchData = async () => {
+    try {
+      const result = await axios(templateUrl);
       setProducts(result.data);
-    };
+  } catch(e) {
+    console.error(e)
+  }};
     fetchData();
   }, []);
     
     return (
-  <ul>
-    {products.map(({ title, id, image, price}) => (
-      <li className="border py-4 px-8 rounded flex flex-col items-center md-2 list-none" key={id}>
+  <div>
+    {products.map(({ title, id, image, price }) => (
+      <div className="border py-4 px-8 rounded flex flex-col items-center md-2 list-none" key={id}>
         <p className="font-bold">{title}</p>
         <img src={image} alt={title} className="w-1/6" />
         <p>{price}</p>
-      </li>
+      </div>
     )
     )}
-  </ul>
+  </div>
   )
     }
   
