fcde15b26c337a91f447d1ea42989483be258add
Kiverok <kiver566@ukr.net>
2022-10-29 15:48:29 +0300

    filterCard

--- src/components/Products.js
+++ src/components/Products.js
@@ -32,12 +32,21 @@ function Products() {
     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     onChange={(e) => setSearch(e.target.value)}
      />
-    <ul>
+     {products.filter(product=>product.title.toLowerCase().includes(search)
+      ).map((product) => (
+      <div className="border py-4 px-8 rounded flex flex-col items-center md-2 list-none" key={product.id}>
+        <p className="font-bold">{product.title}</p>
+        <img src={product.image} alt={product.title} className="w-1/6" />
+        <p>{product.price}</p>
+      </div>
+    )
+    )}
+    {/* <ul>
       {products.filter(product=>product.title.toLowerCase().includes(search)
       ).map((product) => (
         <li key={product.id}>{product.title}</li>
       ))}
-    </ul>
+    </ul> */}
   </div>
   )
     }
