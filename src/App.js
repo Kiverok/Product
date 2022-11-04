783a291b56ff3dfe06251435499596e34acca33e
Kiverok <kiver566@ukr.net>
2022-11-04 19:18:14 +0200

    fetchProducts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import Products from './components/Products';
 
 
 
 
+
+
 function App() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([])
+  // const search = (data) => {
+  //   return data.filter((product) => product.title.toLowerCase().includes(query));
+  // };
+  useEffect(()=>{
+const fetchProducts = async () =>{
+  const res = await axios.get("https://fakestoreapi.com/products?limit=30")
+  setData(res.data);
+};
+fetchProducts()
+  },[])
+  const search = (data) => {
+    return data.filter((item) => item.title.toLowerCase().includes(query));
+  };
   return (
 <div className="container mx-auto max-w-2xl pt-5">
-  <Products />
+<input 
+    type="text" 
+    placeholder="Search..."
+    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    onChange={(e) => setQuery(e.target.value)}
+     />
+   <Products data={search(data)} /> 
 </div>
 )
   }
--- src/components/Products.js
+++ src/components/Products.js
@@ -4,40 +4,33 @@ import axios from "axios";
 
 
 
-function Products() {
-    const [products, setProducts] = useState([]);
-    const [search, setSearch] = useState("");
-    // console.log(products.filter(product=>product.title.toLowerCase().includes("me")))
-  // const url = 'https://fakestoreapi.com/products?limit=5'
-  // const query = "5";
+function Products( { data } ) {
+  //   const [products, setProducts] = useState([]);
+  //   // console.log(products.filter(product=>product.title.toLowerCase().includes("me")))
+  // // const url = 'https://fakestoreapi.com/products?limit=5'
+  // // const query = "5";
+  // // const templateUrl = `https://fakestoreapi.com/products?limit=${query}`
+  // useEffect(() => {
+  // const query = "30";
   // const templateUrl = `https://fakestoreapi.com/products?limit=${query}`
-  useEffect(() => {
-  const query = "30";
-  const templateUrl = `https://fakestoreapi.com/products?limit=${query}`
-  const fetchData = async () => {
-    try {
-      const result = await axios(templateUrl);
-      setProducts(result.data);
-  } catch(e) {
-    console.error(e)
-  }};
-    fetchData();
-  }, []);
+  // const fetchData = async () => {
+  //   try {
+  //     const result = await axios(templateUrl);
+  //     setProducts(result.data);
+  // } catch(e) {
+  //   console.error(e)
+  // }};
+  //   fetchData();
+  // }, []);
     
     return (
   <div>
-    <input 
-    type="text" 
-    placeholder="Search..."
-    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    onChange={(e) => setSearch(e.target.value)}
-     />
-     {products.filter(product=>product.title.toLowerCase().includes(search)
-      ).map((product) => (
-      <div className="border py-4 px-8 rounded flex flex-col items-center md-2 list-none" key={product.id}>
-        <p className="font-bold">{product.title}</p>
-        <img src={product.image} alt={product.title} className="w-1/6" />
-        <p>{product.price}</p>
+ {data.map((item) => (
+      <div className="border py-4 px-8 rounded flex flex-col items-center md-2 list-none" key={item.id}>
+        <p className="font-bold">{item.title}</p>
+        <p>{item.category}</p>
+        <img src={item.image} alt={item.title} className="w-1/6" />
+        <p>{item.price}</p>
       </div>
     )
     )}
